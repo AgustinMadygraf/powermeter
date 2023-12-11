b95cb7cbb1dce6c03703e118e8679e5a21848db6
AgustinMadygraf <agustin.mtto.madygraf>
2023-12-10 21:35:09 -0300

    Update code to use asyncio and telegram API to save chat history to JSON. (generated by blackbox.ai )

--- expo/ChatBot_Telegram.py
+++ expo/ChatBot_Telegram.py
@@ -7,11 +7,54 @@ import requests
 import random
 import sys
 import telegram
+import asyncio
 
+async def act_json():
+    token_telegram = os.getenv('telegram_token')
+    bot = telegram.Bot(token_telegram)
 
+    async with bot:
+        historial = await bot.get_updates()
+
+        # Diccionario para almacenar el historial de cada chat
+        chat_histories = {}
+        user_info = {}
+
+        for update in historial:
+            if update.message:
+                chat_id = update.message.chat.id
+                text = update.message.text
+
+                # Crear una nueva entrada en el diccionario si no existe
+                if chat_id not in chat_histories:
+                    chat_histories[chat_id] = []
+
+                # Agregar el mensaje al historial del chat
+                chat_histories[chat_id].append({
+                    "role": "user",
+                    "content": text
+                })
+
+                # Recolectar la información del usuario
+                user = update.message.from_user
+                if user.id not in user_info:
+                    user_info[user.id] = {
+                        "username": user.username or 'Sin username',
+                        "first_name": user.first_name,
+                        "last_name": user.last_name or '',
+                        "id": user.id
+                    }
+
+        # Guardar el historial de cada chat y la información del usuario en un archivo JSON
+        data_to_save = {
+            "user_info": user_info,
+            "chat_histories": chat_histories
+        }
+
+        with open("context_window_telegram.json", "w", encoding="utf-8") as file:
+            json.dump(data_to_save, file, indent=4, ensure_ascii=False)
 def limpiar_pantalla():
     os.system('cls' if os.name == 'nt' else 'clear')
-
 async def main():
     token_telegram = os.getenv('telegram_token')
     bot = telegram.Bot(token_telegram)
@@ -89,8 +132,6 @@ def cargar_chat_history(file_path):
     except json.JSONDecodeError:
         print(f"Error al leer el archivo: {file_path}. Formato de archivo inválido.")
         return {}, {}, None
-
-
 def iniciar_chat(chat_history, user_info, user_id):
     # Convertir user_id a string si es necesario (JSON keys son strings)
     user_id_str = str(user_id)
@@ -100,15 +141,17 @@ def iniciar_chat(chat_history, user_info, user_id):
         chat_history[user_id_str] = []
 
     while True:
-        prompt = input("Enter a prompt: ")
-        if prompt.lower() == "exit":
-            break
+        prompt = " "
+        asyncio.run(act_json())
+        chat_history, user_info, ultimo_rol = cargar_chat_history(chat_history_path)
+
+        if prompt.lower() == " ":
+            print("esperando ")
+            time.sleep(1)
         else:
             # Agregar el mensaje del usuario al historial del chat
             chat_history[user_id_str].append({"role": "user", "content": prompt})
             procesar_respuesta(chat_history, user_info, user_id_str)
-
-
 def procesar_respuesta(chat_history, user_info, user_id):
     # Definir las variables de tiempo de espera y reintentos
     tiempo_espera_base = 1  # tiempo de espera base en segundos
@@ -138,6 +181,7 @@ def procesar_respuesta(chat_history, user_info, user_id):
             # Agregar la respuesta del asistente al historial del chat
             chat_history[user_id].append({"role": "assistant", "content": full_reply_content})
             print(f"GPT: {full_reply_content}")
+            ###################################################################################
             guardar_chat_history(chat_history, user_info, chat_history_path)
             break
 
@@ -196,8 +240,8 @@ chat_history_path = "context_window_telegram.json"
 
 # Aquí puedes usar chat_history_path para lo que necesites después de la selección
 print(f"El archivo seleccionado para trabajar es: {chat_history_path}")
-time.sleep(4)
-
+time.sleep(1)
+asyncio.run(act_json())
 clave_api = obtener_api_key()
 
 if clave_api is None:
@@ -208,8 +252,6 @@ while True:
     try:
         # Actualiza esta línea para manejar los tres valores devueltos
         chat_history, user_info, ultimo_rol = cargar_chat_history(chat_history_path)
-
-        # Ahora puedes usar `ultimo_rol` para saber si el último mensaje fue de "user" o "assistant"
         if ultimo_rol:
             print(f"El último mensaje en la conversación con el usuario 593052206 fue de un '{ultimo_rol}'.")
 
